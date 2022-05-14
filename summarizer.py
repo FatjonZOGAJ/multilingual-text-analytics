91500e2291c56ede5dd07ab4b25809733b1232c5
Rafael <rafael.sterzinger@gmx.at>
2022-05-14 23:11:09 +0200

    Add function call for summarize

--- summarizer.py
+++ summarizer.py
@@ -1,12 +1,8 @@
-
-import json
 import os
-from azure.ai.textanalytics import TextAnalyticsClient
+from azure.ai.textanalytics import TextAnalyticsClient,ExtractSummaryAction
 from azure.core.credentials import AzureKeyCredential
 
-# Authenticate the client using your key and endpoint 
 def authenticate_client():
-
     endpoint = os.environ["AZURE_LANGUAGE_ENDPOINT"]
     key = os.environ["AZURE_LANGUAGE_KEY"]
 
@@ -18,39 +14,32 @@ def authenticate_client():
 
 client = authenticate_client()
 
-# Example method for summarizing text
-def sample_extractive_summarization(client):
-    from azure.core.credentials import AzureKeyCredential
-    from azure.ai.textanalytics import (
-        TextAnalyticsClient,
-        ExtractSummaryAction
-    )
-
+def summarize(text, max_sentence_count = 4):
     document = [
-        "The extractive summarization feature uses natural language processing techniques to locate key sentences in an unstructured text document. "
-        "These sentences collectively convey the main idea of the document. This feature is provided as an API for developers. "
-        "They can use it to build intelligent solutions based on the relevant information extracted to support various use cases. "
-        "In the public preview, extractive summarization supports several languages. It is based on pretrained multilingual transformer models, part of our quest for holistic representations. "
-        "It draws its strength from transfer learning across monolingual and harness the shared nature of languages to produce models of improved quality and efficiency. "
+        text
     ]
 
     poller = client.begin_analyze_actions(
         document,
         actions=[
-            ExtractSummaryAction(max_sentence_count=4)
+            ExtractSummaryAction(max_sentence_count=max_sentence_count)
         ],
     )
 
     document_results = poller.result()
     for result in document_results:
-        extract_summary_result = result[0]  # first document, first result
+        extract_summary_result = result[0]
         if extract_summary_result.is_error:
             print("...Is an error with code '{}' and message '{}'".format(
                 extract_summary_result.code, extract_summary_result.message
             ))
+            raise RuntimeError()
         else:
-            print("Summary extracted: \n{}".format(
-                " ".join([sentence.text for sentence in extract_summary_result.sentences]))
-            )
+            return " ".join([sentence.text for sentence in extract_summary_result.sentences])
 
-sample_extractive_summarization(client)
+
+print(summarize("The extractive summarization feature uses natural language processing techniques to locate key sentences in an unstructured text document. "
+        "These sentences collectively convey the main idea of the document. This feature is provided as an API for developers. "
+        "They can use it to build intelligent solutions based on the relevant information extracted to support various use cases. "
+        "In the public preview, extractive summarization supports several languages. It is based on pretrained multilingual transformer models, part of our quest for holistic representations. "
+        "It draws its strength from transfer learning across monolingual and harness the shared nature of languages to produce models of improved quality and efficiency.", 2))
