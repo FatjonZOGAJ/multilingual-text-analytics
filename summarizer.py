a8340f39385bea88872bded761335f7a617a69af
Papilou <sfoucher@ethz.ch>
2022-05-15 07:16:00 +0200

    Notebook update

--- summarizer.py
+++ summarizer.py
@@ -37,9 +37,9 @@ def summarize(text, max_sentence_count = 4):
         else:
             return " ".join([sentence.text for sentence in extract_summary_result.sentences])
 
-
-print(summarize("The extractive summarization feature uses natural language processing techniques to locate key sentences in an unstructured text document. "
-        "These sentences collectively convey the main idea of the document. This feature is provided as an API for developers. "
-        "They can use it to build intelligent solutions based on the relevant information extracted to support various use cases. "
-        "In the public preview, extractive summarization supports several languages. It is based on pretrained multilingual transformer models, part of our quest for holistic representations. "
-        "It draws its strength from transfer learning across monolingual and harness the shared nature of languages to produce models of improved quality and efficiency.", 2))
+if __name__ == "__main__":
+    print(summarize("The extractive summarization feature uses natural language processing techniques to locate key sentences in an unstructured text document. "
+            "These sentences collectively convey the main idea of the document. This feature is provided as an API for developers. "
+            "They can use it to build intelligent solutions based on the relevant information extracted to support various use cases. "
+            "In the public preview, extractive summarization supports several languages. It is based on pretrained multilingual transformer models, part of our quest for holistic representations. "
+            "It draws its strength from transfer learning across monolingual and harness the shared nature of languages to produce models of improved quality and efficiency.", 2))
--- translator.py
+++ translator.py
@@ -61,43 +61,43 @@ def translate_de_to_en(data, key, endpoint):
     translated = response[0]['translations'][0]['text']
     return translated
 
+if __name__ == "__main__":
+    # To use on a list, call method on every list item
+    en_data = 'The extractive summarization feature uses natural language processing techniques to locate key sentences in an unstructured text document.'
+    print(translate_en_to_de(en_data, key, endpoint))
 
-# To use on a list, call method on every list item
-en_data = 'The extractive summarization feature uses natural language processing techniques to locate key sentences in an unstructured text document.'
-print(translate_en_to_de(en_data, key, endpoint))
+    de_data = 'Die extraktive Zusammenfassungsfunktion verwendet Techniken zur Verarbeitung natürlicher Sprache, um Schlüsselsätze in einem unstrukturierten Textdokument zu finden.'
+    print(translate_de_to_en(de_data, key, endpoint))
 
-de_data = 'Die extraktive Zusammenfassungsfunktion verwendet Techniken zur Verarbeitung natürlicher Sprache, um Schlüsselsätze in einem unstrukturierten Textdokument zu finden.'
-print(translate_de_to_en(de_data, key, endpoint))
+    ## Translate these sentences
+    document = [
+            "The extractive summarization feature uses natural language processing techniques to locate key sentences in an unstructured text document. "
+            "These sentences collectively convey the main idea of the document. This feature is provided as an API for developers. "
+            "They can use it to build intelligent solutions based on the relevant information extracted to support various use cases. "
+            "In the public preview, extractive summarization supports several languages. It is based on pretrained multilingual transformer models, part of our quest for holistic representations. "
+            "It draws its strength from transfer learning across monolingual and harness the shared nature of languages to produce models of improved quality and efficiency. "
+        ]
 
-## Translate these sentences
-document = [
-        "The extractive summarization feature uses natural language processing techniques to locate key sentences in an unstructured text document. "
-        "These sentences collectively convey the main idea of the document. This feature is provided as an API for developers. "
-        "They can use it to build intelligent solutions based on the relevant information extracted to support various use cases. "
-        "In the public preview, extractive summarization supports several languages. It is based on pretrained multilingual transformer models, part of our quest for holistic representations. "
-        "It draws its strength from transfer learning across monolingual and harness the shared nature of languages to produce models of improved quality and efficiency. "
-    ]
+    ## Document for us can be a collection of paragraphs, currently only one entry
+    for value in document:
+        print(translate_en_to_de(value, key, endpoint))
 
-## Document for us can be a collection of paragraphs, currently only one entry
-for value in document:
-    print(translate_en_to_de(value, key, endpoint))
-
-## Tests on medical data:
-data_path = "translation_data/de-en_txt_data/"
-file_de = "german_data.txt"
+    ## Tests on medical data:
+    data_path = "translation_data/de-en_txt_data/"
+    file_de = "german_data.txt"
 
 
-file_eng = open(data_path+"english_data.txt", 'r')
-for count, line in enumerate(file_eng.readlines()):
-    print(translate_en_to_de(line, key, endpoint))
-    if count==2:
-        break
+    file_eng = open(data_path+"english_data.txt", 'r')
+    for count, line in enumerate(file_eng.readlines()):
+        print(translate_en_to_de(line, key, endpoint))
+        if count==2:
+            break
 
-file_de = open(data_path+"german_data.txt", 'r')
-for count, line in enumerate(file_de.readlines()):
-    print(line)
-    if count==2:
-        break
+    file_de = open(data_path+"german_data.txt", 'r')
+    for count, line in enumerate(file_de.readlines()):
+        print(line)
+        if count==2:
+            break
 
 
 
